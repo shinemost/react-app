c5ed68111adbf70ada72fc963a0b8ab422bebe69
shinemost <supertain147@163.com>
2023-04-26 23:30:10 +0800

    在jsx组件里使用数组map函数循环遍历

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,19 +3,19 @@ import { Fragment } from 'react'
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 22:46:31
- * @LastEditTime: 2023-04-26 23:18:34
+ * @LastEditTime: 2023-04-26 23:28:01
  * @FilePath: \react-app\src\components\ListGroup.tsx
  */
 function ListGroup() {
+  const items = ['纽约', '旧金山', '东京', '伦敦', '巴黎', '北京']
+
   return (
     <Fragment>
       <h1>List Group</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </Fragment>
   )
