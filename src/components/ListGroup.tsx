c4b078ad4b121daf86915633966a141a94932a46
shinemost <supertain147@163.com>
2023-04-26 23:19:02 +0800

    使用Fragment代码片段包裹返回多个元素

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,12 +3,12 @@ import { Fragment } from 'react'
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 22:46:31
- * @LastEditTime: 2023-04-26 23:16:20
+ * @LastEditTime: 2023-04-26 23:18:34
  * @FilePath: \react-app\src\components\ListGroup.tsx
  */
 function ListGroup() {
   return (
-    <>
+    <Fragment>
       <h1>List Group</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
@@ -17,7 +17,7 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
-    </>
+    </Fragment>
   )
 }
 
