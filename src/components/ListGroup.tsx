e65a2c8b968c0926aa70e6d9e4e5ff2030932fb8
shinemost <supertain147@163.com>
2023-04-27 22:28:55 +0800

    使用<>简化代码片段

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,14 +3,14 @@ import { Fragment } from 'react'
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 22:46:31
- * @LastEditTime: 2023-04-27 22:16:36
+ * @LastEditTime: 2023-04-27 22:23:49
  * @FilePath: \react-app\src\components\ListGroup.tsx
  */
 function ListGroup() {
   const items = ['纽约', '旧金山', '东京', '伦敦', '巴黎', '北京']
 
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item, index) => (
@@ -23,7 +23,7 @@ function ListGroup() {
           </li>
         ))}
       </ul>
-    </Fragment>
+    </>
   )
 }
 
