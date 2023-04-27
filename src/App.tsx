91b8d7316cdcb5be9652800b7849f1d37f3247f5
shinemost <supertain147@163.com>
2023-04-27 23:19:36 +0800

    通过props传递数据，学习ts的接口

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 20:47:45
- * @LastEditTime: 2023-04-27 22:56:43
+ * @LastEditTime: 2023-04-27 23:16:25
  * @FilePath: \react-app\src\App.tsx
  */
 import ListGroup from './components/ListGroup'
 
 function App() {
+  let items = ['纽约', '旧金山', '东京', '伦敦', '巴黎', '北京']
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="城市" />
     </div>
   )
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,18 +3,22 @@ import { MouseEvent, useState } from 'react'
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 22:46:31
- * @LastEditTime: 2023-04-27 22:50:51
+ * @LastEditTime: 2023-04-27 23:15:20
  * @FilePath: \react-app\src\components\ListGroup.tsx
  */
-function ListGroup() {
-  let items = ['纽约', '旧金山', '东京', '伦敦', '巴黎', '北京']
 
+interface Props {
+  items: string[]
+  heading: string
+}
+function ListGroup({ items, heading }: Props) {
   //状态钩子
   const [selectedIndex, setSelectdIndex] = useState(-1)
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
