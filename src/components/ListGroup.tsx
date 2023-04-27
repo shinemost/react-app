8e8bfc6a68b7a83af2666c867c7e6b116bca1946
shinemost <supertain147@163.com>
2023-04-27 22:53:28 +0800

    修改组件的状态，使用react内置函数useState，告知react该组件状态或数据可能发生变化

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
-import { Fragment } from 'react'
+import { MouseEvent, useState } from 'react'
 
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 22:46:31
- * @LastEditTime: 2023-04-27 22:23:49
+ * @LastEditTime: 2023-04-27 22:50:51
  * @FilePath: \react-app\src\components\ListGroup.tsx
  */
 function ListGroup() {
-  const items = ['纽约', '旧金山', '东京', '伦敦', '巴黎', '北京']
+  let items = ['纽约', '旧金山', '东京', '伦敦', '巴黎', '北京']
+
+  //状态钩子
+  const [selectedIndex, setSelectdIndex] = useState(-1)
 
   return (
     <>
@@ -15,9 +18,13 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
             key={item}
-            onClick={(event) => console.log(event)}
+            onClick={() => setSelectdIndex(index)}
           >
             {item}
           </li>
