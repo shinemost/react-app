b9df37c7ee42ec8019f8046f75746ba8482468e8
shinemost <supertain147@163.com>
2023-04-29 01:12:32 +0800

    将按钮和警告两个组件结合起来，点击按钮弹出警告，点击警告关闭按钮关闭警告

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 20:47:45
- * @LastEditTime: 2023-04-28 23:46:00
+ * @LastEditTime: 2023-04-29 01:07:55
  * @FilePath: \react-app\src\App.tsx
  */
+import { useState } from 'react'
 import Alert from './components/Alert'
 import Button from './components/Button'
 
 function App() {
+  const [visible, setVisible] = useState(false)
+
   return (
     <div>
-      <Alert>
-        泰裤 <h1>辣</h1>
-      </Alert>
-      <Button color="danger" onclick={() => console.log('clicked')}>
-        Primary
+      {visible && <Alert onClose={() => setVisible(false)}> 泰裤辣</Alert>}
+      <Button color="danger" onclick={() => setVisible(true)}>
+        My Button
       </Button>
     </div>
   )
