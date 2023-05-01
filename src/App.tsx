407338c74f3b9c0e3a5e35e10050cee25833da35
fuhaijing <fuhaijing@gotion.com.cn>
2023-05-01 16:42:13 +0800

    将 JSX 作为子组件传递

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@
  * @LastEditTime: 2023-05-01 14:28:11
  * @FilePath: \react-app\src\App.tsx
  */
-import { useState } from 'react'
-import Mybutton from './components/MyButton'
+import Avatar from './components/Avatar'
+
+function Card({ children }: any) {
+  return <div className="card">{children}</div>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1)
-  }
-
   return (
-    <div>
-      <Mybutton count={count} onClick={handleClick} />
-      <Mybutton count={count} onClick={handleClick} />
-    </div>
+    <Card>
+      <Avatar
+        size={100}
+        person={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2',
+        }}
+      />
+    </Card>
   )
 }
 
