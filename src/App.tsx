2d87da60d24711404690b3d2c7562ca18643d99b
shinemost <supertain147@163.com>
2023-05-02 12:02:13 +0800

    使用if语句和变量另一种重构，通过props进行父子组件数据传递

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 20:47:45
- * @LastEditTime: 2023-05-02 10:11:05
+ * @LastEditTime: 2023-05-02 10:36:15
  * @FilePath: \react-app\src\App.tsx
  */
 import Drink from './components/Drink'
@@ -9,8 +9,13 @@ import Drink from './components/Drink'
 function App() {
   return (
     <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
+      <Drink name="tea" content="15–70 mg/cup" part="leaf" age="4,000+ years" />
+      <Drink
+        name="coffee"
+        content="80–185 mg/cup"
+        part="bean"
+        age="1,000+ years"
+      />
     </div>
   )
 }
--- src/components/Drink.tsx
+++ src/components/Drink.tsx
@@ -1,15 +1,4 @@
-export default function ({ name }: any) {
-  let part, content, age
-
-  if (name === 'tea') {
-    part = 'leaf'
-    content = '15–70 mg/cup'
-    age = '4,000+ years'
-  } else if (name === 'coffee') {
-    part = 'bean'
-    content = '80–185 mg/cup'
-    age = '1,000+ years'
-  }
+export default function ({ name, part, content, age }: any) {
   return (
     <section>
       <h1>{name}</h1>
