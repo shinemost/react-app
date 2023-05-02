7facbb6edd803d9d5fe64c3fe0a6b2c85f3d89e4
shinemost <supertain147@163.com>
2023-05-02 10:15:16 +0800

    使用if语句和变量重构之前准备

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 /*
- * @LastEditors: fuhaijing fuhaijing@gotion.com.cn
+ * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 20:47:45
- * @LastEditTime: 2023-05-01 14:28:11
+ * @LastEditTime: 2023-05-02 10:11:05
  * @FilePath: \react-app\src\App.tsx
  */
-import Avatar from './components/Avatar'
-
-function Card({ children }: any) {
-  return <div className="card">{children}</div>
-}
+import Drink from './components/Drink'
 
 function App() {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2',
-        }}
-      />
-    </Card>
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
   )
 }
 
