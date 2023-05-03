4fc968d1612d84f710e94a5dc9bb443e09ecfb0f
shinemost <supertain147@163.com>
2023-05-03 10:43:24 +0800

    捕获阶段事件

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -2,7 +2,7 @@ import Button from './Button'
 
 const Toolbar = () => {
   return (
-    <div className="Toolbar" onClick={() => alert('你点击了toolbar')}>
+    <div className="Toolbar" onClickCapture={() => alert('你点击了下面的按钮')}>
       <Button onclick={() => alert('正在播放！')}>播放电影</Button>
       <Button onclick={() => alert('正在上传！')}>上传图片</Button>
     </div>
