612b787a86f827184aa72b57ffe4cbf53c76ce03
shinemost <supertain147@163.com>
2023-05-03 11:57:29 +0800

    组件里定义多个状态

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 20:47:45
- * @LastEditTime: 2023-05-03 10:46:25
+ * @LastEditTime: 2023-05-03 11:56:13
  * @FilePath: \react-app\src\App.tsx
  */
-import Signup from './components/SignUp'
+/*
+ * @LastEditors: shinemost supertain147@163.com
+ * @Date: 2023-04-26 20:47:45
+ * @LastEditTime: 2023-05-03 11:55:12
+ * @FilePath: \react-app\src\App.tsx
+ */
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <div>
-      <Signup />
+      <Gallery />
     </div>
   )
 }
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,42 @@
+/*
+ * @LastEditors: shinemost supertain147@163.com
+ * @Date: 2023-05-03 11:52:51
+ * @LastEditTime: 2023-05-03 11:53:09
+ * @FilePath: \react-app\src\components\Gallery.tsx
+ */
+import { useState } from 'react'
+import { sculptureList } from '../data/data2.js'
+
+const Gallery = () => {
+  const [index, setIndex] = useState(0)
+  const [showMore, setShowMore] = useState(false)
+
+  function handleNextClick() {
+    setIndex(index + 1)
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore)
+  }
+
+  let sculpture = sculptureList[index]
+  return (
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  )
+}
+
+export default Gallery
