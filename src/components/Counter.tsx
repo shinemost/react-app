22476cb071b25b3894e29c33938128d8b5ad14dc
shinemost <supertain147@163.com>
2023-05-07 23:03:43 +0800

    批量更新number

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,9 +2,8 @@
  * @Author: fuhj_smc fuhj_smc@si-tech.com.cn
  * @Date: 2023-05-07 21:32:31
  * @LastEditors: shinemost supertain147@163.com
- * @LastEditTime: 2023-05-07 23:01:27
+ * @LastEditTime: 2023-05-07 23:03:20
  * @FilePath: \react-app\src\components\Counter.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { useState } from 'react'
 
