bb436aeb9b2ac63b5ee7c525253f9543674be464
shinemost <supertain147@163.com>
2023-05-07 23:02:18 +0800

    批量更新number

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,7 +2,7 @@
  * @Author: fuhj_smc fuhj_smc@si-tech.com.cn
  * @Date: 2023-05-07 21:32:31
  * @LastEditors: shinemost supertain147@163.com
- * @LastEditTime: 2023-05-07 22:51:39
+ * @LastEditTime: 2023-05-07 23:01:27
  * @FilePath: \react-app\src\components\Counter.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,12 +15,11 @@ const Counter = () => {
       <h1>{number}</h1>
       <button
         onClick={() => {
-          setNumber((number) => number + 1)
-          setNumber((number) => number + 1)
+          setNumber(number + 5)
           setNumber((number) => number + 1)
         }}
       >
-        +3
+        增加数字
       </button>
     </>
   )
