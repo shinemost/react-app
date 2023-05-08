3b525d5b834b13825ad8d8aa491b5cc1c2804d99
shinemost <supertain147@163.com>
2023-05-08 21:06:02 +0800

    将useState(xx)中xx对象看做不可变的，不可以直接修改xx对象的属性，这被称为突变，而应该使用setXX

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @LastEditors: shinemost supertain147@163.com
  * @Date: 2023-04-26 20:47:45
- * @LastEditTime: 2023-05-07 23:37:14
+ * @LastEditTime: 2023-05-08 21:02:55
  * @FilePath: \react-app\src\App.tsx
  */
 /*
@@ -10,8 +10,7 @@
  * @LastEditTime: 2023-05-03 11:55:12
  * @FilePath: \react-app\src\App.tsx
  */
-import { number } from 'prop-types'
-import { getFinalState } from './js/processQueue.js'
+import MovingDot from './components/MovingDot.js'
 
 function increment(n) {
   return n + 1
@@ -20,50 +19,10 @@ increment.toString = () => 'n => n+1'
 
 function App() {
   return (
-    <>
-      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[increment, increment, increment]}
-        expected={3}
-      />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment]} expected={6} />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
-    </>
+    <div>
+      <MovingDot />
+    </div>
   )
-}
-
-function TestCase({ baseState, queue, expected }) {
-  const actual = getFinalState(baseState, queue)
-  return (
-    <>
-      <p>
-        初始 state：<b>{baseState}</b>
-      </p>
-      <p>
-        队列：<b>[{queue.join(', ')}]</b>
-      </p>
-      <p>
-        预期结果：<b>{expected}</b>
-      </p>
-      <p
-        style={{
-          color: actual === expected ? 'green' : 'red',
-        }}
-      >
-        你的结果：<b>{actual}</b> ({actual === expected ? '正确' : '错误'})
-      </p>
-    </>
-  )
-}
-
-TestCase.propTypes = {
-  baseState: number,
-  queue: [],
-  expected: number,
 }
 
 export default App
